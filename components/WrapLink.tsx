37eb190ed92987beef2a9ea01aeb65cdb0ed7ca0
dmamira <miramami@outlook.jp>
2020-09-28 21:30:13 +0900

    refactoring

--- components/WrapLink.tsx
+++ components/WrapLink.tsx
@@ -8,18 +8,16 @@ const WrapLink = ({ href, ...rest }) => {
     let baseURI_as = href;
     // make absolute url relative
     // when displayed in url bar
-    if (
-      typeof document !== "undefined" &&
-      baseURI_as.startsWith("/") &&
-      isIPFS.ipfsUrl(document.baseURI)
-    ) {
+    if (baseURI_as.startsWith("/")) {
       //  for static html compilation
       baseURI_as = "." + href;
       // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
       // on the client
       //   document is unavailable when compiling on the server
-      baseURI_as = resolve(document.baseURI, baseURI_as);
-      // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
+      if (typeof document !== "undefined") {
+        baseURI_as = resolve(document.baseURI, baseURI_as);
+        // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
+      }
     }
     return baseURI_as;
   }, [href]);
