5f7cf83a17235e01727be54d1d63e55824d9515b
dmamira <miramami@outlook.jp>
2020-09-28 21:14:03 +0900

    fixed undifined bug

--- components/WrapLink.tsx
+++ components/WrapLink.tsx
@@ -1,6 +1,6 @@
 import { resolve } from "url";
 import React, { useMemo } from "react";
-import isIPFS from 'is-ipfs';
+import isIPFS from "is-ipfs";
 import Link from "next/link";
 
 const WrapLink = ({ href, ...rest }) => {
@@ -8,16 +8,18 @@ const WrapLink = ({ href, ...rest }) => {
     let baseURI_as = href;
     // make absolute url relative
     // when displayed in url bar
-    if (baseURI_as.startsWith("/") && isIPFS.ipfsUrl(document.baseURI)) {
+    if (
+      typeof document !== "undefined" &&
+      baseURI_as.startsWith("/") &&
+      isIPFS.ipfsUrl(document.baseURI)
+    ) {
       //  for static html compilation
       baseURI_as = "." + href;
       // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
       // on the client
       //   document is unavailable when compiling on the server
-      if (typeof document !== "undefined") {
-        baseURI_as = resolve(document.baseURI, baseURI_as);
-        // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
-      }
+      baseURI_as = resolve(document.baseURI, baseURI_as);
+      // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
     }
     return baseURI_as;
   }, [href]);
